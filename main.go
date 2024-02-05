5dd650a6ea90a74d5c1fc4f253580f3cce471111
KPatiwat <Patiwatkrubst@hotmail.com>
2024-02-05 14:34:04 +0700

    init project

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, Fiber!")
+	})
+
+	log.Fatalln(app.Listen(":8080"))
+}
